85992fa5be1a19de758a219e0b85e8e27afc74e6
ubaidubai <ubaidubai.art@gmail.com>
2026-03-19 00:17:45 +0800

    Fixed LoadNextScene in PlayerTrigger.cs

--- Scripts/Player/PlayerTrigger.cs
+++ Scripts/Player/PlayerTrigger.cs
@@ -20,44 +20,42 @@ public sealed class PlayerTrigger : SystemBase
 [System(SystemGroup.PostPhysics, SystemRunMode.PlayOnly)]
 public class PlayerTriggerHandler : TriggerSystemBase
 {
-    private const string SourceSceneName = "Level_One";
     private const string endSceneName = "Scenes/EndScene.scn";
     protected override void OnTriggerEnter(Entity self, TriggerEvent evt)
     {
         Entity other = Entity.FromId(World!, evt.OtherEntityId);
 
-
-        if (Entity.FromId(World!, self.Id).TryGetComponent<PlayerTriggerComponent>(out PlayerTriggerComponent squidWard))
+        if (self.HasComponent<PlayerComponent>() ||
+            other.HasComponent<PlayerComponent>())
         {
-            //Log("Trigger: I have a Squidward!");
+            if ((other.TryGetComponent<MatchSignifierComponent>(out MatchSignifierComponent mSign) && other.GetComponent<MatchSignifierComponent>().signifierID == 86118001) ||
+                (self.TryGetComponent<MatchSignifierComponent>(out MatchSignifierComponent mSign2) && self.GetComponent<MatchSignifierComponent>().signifierID == 86118001))
+            {
+                Log("Endscene Entering. . . ");
+                SceneManager sceneManager = SceneManager.Instance;
+                //Loadscene use dalton's
+                sceneManager.SetNextAudioTransition(2.0f, true);
+                //var scene = SceneManager.Instance.LoadScene(TargetScenePath);
+                //Like creating a new scene / allocate a new scene in the registry
+                var sceneIndex = SceneManager.Instance.AddScene();
+                var ss = SceneManager.Instance.LoadScene(sceneIndex, endSceneName);
+                SceneManager.Instance.SetActive(sceneIndex);
+            }
         }
-        else
+        
+        if (self.HasComponent<PlayerTriggerComponent>() ||
+                other.HasComponent<PlayerTriggerComponent>())
         {
-            //Log("Trigger: I DONT have a Squidward!");
-            return;
-        }
-
-        if (other.TryGetComponent<MatchSignifierComponent>(out MatchSignifierComponent mSign) && other.GetComponent<MatchSignifierComponent>().signifierID == 86118001 &&
-            InventoryController.ms02_Count >= 0 && InventoryController.ms01_Count >= 0)
-        {
-            Log("Endscene Entering. . . ");
-            SceneManager sceneManager = SceneManager.Instance;
-            //Loadscene use dalton's
-            sceneManager.SetNextAudioTransition(2.0f, true);
-            //var scene = SceneManager.Instance.LoadScene(TargetScenePath);
-            //Like creating a new scene / allocate a new scene in the registry
-            var sceneIndex = SceneManager.Instance.AddScene();
-            var ss = SceneManager.Instance.LoadScene(sceneIndex, endSceneName);
-            SceneManager.Instance.SetActive(sceneIndex);
+            //if I detect a marine snow obj, send it back to whence it came!
+            if (Entity.FromId(World!, other.Id).TryGetComponent<MS_IDComponent>(out MS_IDComponent msM))
+            {
+                if (msM.collisionCooldown > 0) return; // if still cooling down, dont pick it up
+                AudioManager.instance.PlaySFX("SFX003");
+                //MS_Manager.instance.SendToPool(other.Id);
+                InventoryController.instance.AddToInventory(other.GetComponent<MS_IDComponent>().msID, other.Id);
+            }
         }
        
-        //if I detect a marine snow obj, send it back to whence it came!
-        if (Entity.FromId(World!, other.Id).TryGetComponent<MS_IDComponent>(out MS_IDComponent msM))
-        {
-            if (msM.collisionCooldown > 0) return; // if still cooling down, dont pick it up
-            AudioManager.instance.PlaySFX("SFX003");
-            //MS_Manager.instance.SendToPool(other.Id);
-            InventoryController.instance.AddToInventory(other.GetComponent<MS_IDComponent>().msID, other.Id);
-        }
+
     }
 }
